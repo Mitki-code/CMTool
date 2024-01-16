6d22fe96d81d0f1aeb35e0b09b314d623305d5dc
Mitki-code <xddhezhangyang@outlook.com>
2024-01-16 21:15:44 +0800

    添加项目文件。

---
+++ CMTool/ViewModels/Windows/SubWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CMTool.Services;
+using CMTool.Views.Windows;
+
+namespace CMTool.ViewModels.Windows
+{
+    public partial class SubWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _applicationTitle = "CMTool - Dev";
+
+        private readonly WindowsProviderService _windowsProviderService;
+        public SubWindowViewModel(WindowsProviderService windowsProviderService)
+        {
+            _windowsProviderService = windowsProviderService;
+        }
+
+        [RelayCommand]
+        private void OnOpenWindow()
+        {
+            ///ApplicationHostService.HandleActivationAsyncMain();
+            _windowsProviderService.Show<MainWindow>();
+        }
+    }
+}
