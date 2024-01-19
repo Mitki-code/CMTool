63af8a26b78154f7eaf471c0a01df804af554f1a
Mitki-code <xddhezhangyang@outlook.com>
2024-01-19 17:37:48 +0800

    修复版本号显示Bug

--- CMTool/ViewModels/Settings/AboutViewModel.cs
+++ CMTool/ViewModels/Settings/AboutViewModel.cs
@@ -9,9 +9,10 @@ namespace CMTool.ViewModels.Settings
 {
     public partial class AboutViewModel : ObservableObject
     {
+        private static string appVersionO = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
 
         [ObservableProperty]
-        private string _appVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+        private string _appVersion = appVersionO.Remove(appVersionO.LastIndexOf("0"), 1).Remove(appVersionO.LastIndexOf("."), 1);
 
     }
 
