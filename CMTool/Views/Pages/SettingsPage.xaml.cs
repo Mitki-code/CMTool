6e20b8ea41b8edde2e0e6c6a2107d3a048fce382
Mitki-code <xddhezhangyang@outlook.com>
2024-05-01 16:34:48 +0800

    2.3

--- CMTool/Views/Pages/SettingsPage.xaml.cs
+++ CMTool/Views/Pages/SettingsPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
-// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
-// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
-// All Rights Reserved.
-
-using CMTool.ViewModels.Pages;
+﻿using CMTool.ViewModels.Pages;
 using Microsoft.Extensions.DependencyInjection;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
