e5f64647edfd74d4e6598853a09ee7c66fe68925
miti-code <xddhezhangyang@outlook.com>
2024-05-21 14:10:46 +0800

    20240521

--- CMTool/Module/AutoUpdate.cs
+++ CMTool/Module/AutoUpdate.cs
@@ -1,5 +1,4 @@
 ﻿using Downloader;
-using Octokit;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,39 +9,61 @@ using System.Reflection;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Reflection.PortableExecutable;
+using RestSharp;
+using Newtonsoft.Json.Linq;
 
 namespace CMTool.Module
 {
     internal class AutoUpdate
     {
-        private static GitHubClient client = new(new ProductHeaderValue("CMTool"));
-        private static Credentials tokenAuth = new("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");
+        //private static GitHubClient client = new(new ProductHeaderValue("CMTool"));
+        //private static Credentials tokenAuth = new("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");
         internal static string newVer = "";
         internal static string newUrl = "";
 
+        //Coding 令牌 token be8d9d53c53b86ed60df077bb0aadc66fe7a4b8f
+
+        //private static RestRequest Header()
+        //{
+        //    var request = new RestRequest();
+        //    request.Method = Method.Post;
+        //    request.AddHeader("Authorization", "token be8d9d53c53b86ed60df077bb0aadc66fe7a4b8f");
+        //    request.AddHeader("Content-Type", "application/json");
+        //    request.AddHeader("Accept", "application/json");
+        //    return request;
+        //}
+
+        private static async Task<JObject> GetWebResponse(string url,string name,object value,ParameterType type)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            request.AddHeader("Authorization", "token be8d9d53c53b86ed60df077bb0aadc66fe7a4b8f");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter(name,value,type);
+            var response = await client.PostAsync(request);
+            return JObject.Parse(response.Content);
+        }
+
         internal static async Task<bool> Check(string appVersion)
         {
-            client.Credentials = tokenAuth;
-            var releases = await client.Repository.Release.GetAll("Mitki-code", "CMTool");
-            var latest = releases[0];
-            newVer = latest.Name;
-            newUrl = latest.Assets[0].BrowserDownloadUrl;
+            var postProject = await GetWebResponse("https://e.coding.net/open-api/?Action=DescribeProjectByName&action=DescribeProjectByName",
+                "application/json", "{\n  \"ProjectName\": \"CMTool\"\n}", ParameterType.RequestBody);
+            var response = await GetWebResponse("https://e.coding.net/open-api/?Action=DescribeArtifactRepositoryList&action=DescribeArtifactRepositoryList",
+                "application/json", "{\n  \"PageNumber\": 1,\n  \"PageSize\": 10,\n  \"ProjectId\": "+ postProject["Response"]["Project"]["Id"] + ",\n  \"Type\": 2\n}", ParameterType.RequestBody);
 
-            if (latest.Name != "v"+ appVersion || latest.Name != appVersion)
-                return true;
+
             return false;
+
         }
 
         internal static async Task Down()
         {
             var downloader = new DownloadService();
 
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(newUrl);
-            response.EnsureSuccessStatusCode();
-            string responseUri = response.RequestMessage.RequestUri.ToString();
-
-            await downloader.DownloadFileTaskAsync(responseUri, AppDomain.CurrentDomain.BaseDirectory + @"temp");
+            await downloader.DownloadFileTaskAsync(newVer, AppDomain.CurrentDomain.BaseDirectory + @"temp");
         }
     }
 }
